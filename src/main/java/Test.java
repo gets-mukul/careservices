1e7c24c7b7069332edf93b4c1ac426f55ce4f4fa
gets_mukul <mukulpraj9@gmail.com>
2018-03-14 20:53:35 +0530

    employee dashboard done

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,73 +1,50 @@
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.TypedQuery;
-
-import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import com.careservices.dao.CareUser;
+import com.careservices.dao.CareUserDAO;
 import com.careservices.dao.ClientTrail;
-import com.careservices.dao.EmployeeTask;
-import com.careservices.dao.EmployeeTaskDAO;
 import com.careservices.dao.HibernateSessionFactory;
 import com.careservices.dao.Segment;
-import com.careservices.services.SMSUtility;
-
-import javafx.scene.control.Separator;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// sessionSave();
-		testHQL();
-
-	}
-
-	private static void testSQL() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void testForLoop() {
-		long time = System.currentTimeMillis();
-		List<EmployeeTask> tasks = new EmployeeTaskDAO().findAll();
-		for (EmployeeTask t : tasks) {
-
-		}
-
-	}
-
-	private static void testHQL() {
-		Session groupSession = HibernateSessionFactory.getSession();
-		SMSUtility utility = new SMSUtility();
-
-		String hql = "from Segment where id=:id or parent_id=:id";
-		Query query = groupSession.createQuery(hql);
-		query.setParameter("id", 9);
+	public static void main(String[] args) throws ParseException  {
+		//do object ajj ki date ka object and kal ke date ka object useinig calander utility
 		
-		List<Segment> list = query.list();  
-			
-		for (Segment segment : list) {
-			utility.removeGroup(segment.getGroupId());
-		}
-	}
-		
-
-	private static void sessionSave() {
-		// TODO Auto-generated method stub
 		Session session = HibernateSessionFactory.getSession();
-		ClientTrail ct = new ClientTrail();
-		Transaction orgTransaction = null;
-		try {
-			orgTransaction = session.beginTransaction();
-			session.save(ct);
-			orgTransaction.commit();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			if (orgTransaction != null)
-				orgTransaction.rollback();
+		JSONArray jArr = new JSONArray();
+		Integer employeeId = 4;
+		CareUser employee = new CareUserDAO().findById(employeeId);
+		String hql = "from ClientTrail  WHERE trailStartDate between :currentDate AND :nextDate AND status='TRIAL' AND relatedTask.actor =:actor order by trailStartDate, time";
+		String currentDate = "2018-02-28";
+		String nextDate = "2018-02-28";
+		Date cDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDate); 
+		Date nDate = new SimpleDateFormat("yyyy-MM-dd").parse(nextDate);
+		Query query = session.createQuery(hql);
+		query.setParameter("currentDate", cDate);
+		query.setParameter("nextDate",nDate );
+		query.setParameter("actor", employee);
+		//query.setParameter("status", TaskStatusConstants.TRIAL);
+		List<ClientTrail>trials  = query.list();
+		for (ClientTrail clientTrail : trials) {
+			System.out.println(clientTrail.getId());
 		}
+		
+		
+
 	}
+	
 }
--- src/main/java/com/careservices/rest/api/dashboard/employee/RestEmployeeDashboardApi.java
+++ src/main/java/com/careservices/rest/api/dashboard/employee/RestEmployeeDashboardApi.java
@@ -1,9 +1,30 @@
 package com.careservices.rest.api.dashboard.employee;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.careservices.dao.CareUser;
+import com.careservices.dao.CareUserDAO;
+import com.careservices.dao.ClientTrail;
+import com.careservices.dao.HibernateSessionFactory;
+import com.careservices.dao.Segment;
 
 @Path("dashboard/employee/")
 public class RestEmployeeDashboardApi {
@@ -11,10 +32,47 @@ public class RestEmployeeDashboardApi {
 	@Path("/upcoming_events/{employee_id}")
 	@GET
 	@Produces("application/json")
-	public Response getUpcomingEventsForEmployee()
-	{
-		//find all the trials having date as current date and next day date and status ='TRIAL'
+	public Response getUpcomingEventsForEmployee(@PathParam("employee_id") Integer employeeId) throws ParseException {
+		// find all the trials having date as current date and next day date and status ='TRIAL'
 		
-		return null;
+		JSONObject jsonObj = new JSONObject();
+		Session session = HibernateSessionFactory.getSession();
+		CareUser employee = new CareUserDAO().findById(employeeId);
+		LocalDate localDate = LocalDate.now();
+		String currentDate = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(localDate);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Calendar today = Calendar.getInstance();
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = format.parse(currentDate);
+		today.setTime(date);
+		today.add(Calendar.DAY_OF_YEAR, 1);
+		String nextDate = format.format(today.getTime());
+		
+		
+		String hql = "from ClientTrail  WHERE trailStartDate between :currentDate AND :nextDate AND status='TRIAL' AND relatedTask.actor =:actor order by trailStartDate, time";
+		Query query = session.createQuery(hql);
+		
+		Date cDate = new SimpleDateFormat("yyyy-MM-dd").parse(currentDate); 
+		Date nDate = new SimpleDateFormat("yyyy-MM-dd").parse(nextDate);
+		
+		query.setParameter("currentDate", cDate);
+		query.setParameter("nextDate",nDate );
+		query.setParameter("actor", employee);
+		
+		List<ClientTrail> list = query.list();
+		JSONArray jsonArray = new JSONArray();
+		for (ClientTrail clientTrail : list) {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("id", clientTrail.getId());
+			jsonObject.put("start_date", sdf.format(clientTrail.getTrailStartDate()));
+			jsonObject.put("end_date", sdf.format(clientTrail.getTrailEndDate()));
+			jsonObject.put("time", clientTrail.getTime());
+			jsonArray.put(jsonObject);
+		}
+		JSONObject jObj = new JSONObject();
+		jObj.put("records", jsonArray);
+
+		return Response.status(200).entity(jObj.toString()).header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").allow("OPTIONS").build();
 	}
 }
